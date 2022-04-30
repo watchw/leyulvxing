b9e8830a9a567b383c7886b4f72a59a11215752f
xiangxia <395751618@qq.com>
2022-04-30 12:42:52 +0800

    detail

--- app/src/main/java/com/example/qimo/Life/LifeDetailActivity.java
+++ app/src/main/java/com/example/qimo/Life/LifeDetailActivity.java
@@ -5,6 +5,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.qimo.Base.BaseActivity;
+import com.example.qimo.Model.Life;
 import com.example.qimo.R;
 
 import butterknife.BindView;
@@ -33,6 +34,10 @@ public class LifeDetailActivity extends BaseActivity {
     TextView phoneTextView;
     @BindView(R.id.address_text_view)
     TextView addressTextView;
+    @BindView(R.id.recommend_title_text_view)
+    TextView recommendTitleTextView;
+
+    private Life life;
 
     @Override
     public int bindContentView() {
@@ -41,10 +46,44 @@ public class LifeDetailActivity extends BaseActivity {
 
     @Override
     public void initActivity() {
+        life = getIntent().getParcelableExtra("data");
 
+        imageView.setImageResource(life.img);
+        titleTextView.setText(life.title);
+        if (null != life.detail) {
+            detailTextView.setText(life.detail);
+        }
+        if (null != life.star) {
+            starTextView.setText(life.star);
+        }
+        if (null != life.time) {
+            timeTextView.setText(life.time);
+        }
+        if (null != life.keywords) {
+            keywordsTextView.setText(life.keywords);
+        }
+        if (null != life.recommend) {
+            recommendTextView.setText(life.recommend);
+        }
+        if (null != life.recommend_title) {
+            recommendTitleTextView.setText(life.recommend_title);
+        }
+        if (null != life.phone) {
+            phoneTextView.setText(life.phone);
+        }
+        if (null != life.address) {
+            addressTextView.setText(life.address);
+        }
     }
 
     @OnClick(R.id.phone_text_view)
     public void onClick() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
